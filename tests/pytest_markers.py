22eb553912596ed6e795d84dbdfff7684abc9cd8
Lucas Cimon <lucas.cimon@gmail.com>
2018-02-21 14:25:20 +0100

    Fixing pylint hook

--- tests/pytest_markers.py
+++ tests/pytest_markers.py
@@ -2,5 +2,7 @@ import pytest
 
 pypi_integration = pytest.mark.skipif(
     not pytest.config.getoption('--run-pypi-integration-tests'),
-    reason='Require to create HTTPT connexions to Pypi'
+    reason='Require to create HTTP connexions to Pypi'
 )
+
+# TODO: add Nexus integration tests
