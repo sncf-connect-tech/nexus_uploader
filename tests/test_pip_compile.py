0e80d9cd6a840ffd25ec95bcb7617f17c99ec224
Lucas Cimon <lucas.cimon@gmail.com>
2018-02-21 13:01:23 +0100

    Adding a test

--- tests/test_pip_compile.py
+++ tests/test_pip_compile.py
@@ -5,6 +5,8 @@ try: # Python 3
     from unittest.mock import patch
 except ImportError: # Python 2
     from mock import patch
+from piptools.exceptions import NoCandidateFound
+import pytest
 from .pytest_markers import pypi_integration
 
 
@@ -23,6 +25,15 @@ def test_pipcompile_with_mocked_repository(FakePypiRepository):
                                append_egg_hash_to_url_if_need_be=raise_if_called)
         assert read_requirements(reqs_gen) == \
             [('version-locked', ('lib1', '1.2.3', '')), ('version-locked', ('lib2', '4.5.7', 'via lib1'))]
+
+
+@pypi_integration  # only ran with --run-pypi-integration-tests
+def test_pipcompile_conflicting_dependencies():
+    with pytest.raises(NoCandidateFound) as excinfo:
+        pip_compile(['nexus_uploader==1.0.0','nexus_uploader==1.0.1'],
+                    nexus_hostname='dummy-nexus.wtf',
+                    append_egg_hash_to_url_if_need_be=raise_if_called)
+    assert 'Could not find a version that matches nexus-uploader==1.0.0,==1.0.1' in str(excinfo.value)
 
 
 def raise_if_called(*args, **kwargs):
